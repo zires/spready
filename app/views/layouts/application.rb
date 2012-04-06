8d443ce1bbc49e48f80e59b946fd733fce5814b3
zires <zshuaibin@gmail.com>
2012-04-07 01:05:22 +0800

    Add some general methods

--- app/views/layouts/application.rb
+++ app/views/layouts/application.rb
@@ -1,16 +1,23 @@
 module Layouts
   class Application < ::Stache::View
-    include Sprockets::Helpers::RailsHelper
 
     def stylesheet(name)
-      stylesheet_link_tag name
+      theme_stylesheet_link_tag name, :theme => current_theme
     end
     alias_method :css, :stylesheet
 
     def javascript(name)
-      javascript_include_tag name
+      theme_javascript_include_tag name, :theme => current_theme
     end
     alias_method :js, :javascript
+
+    def compiled(name)
+      theme_compiled_path(name, current_theme)
+    end
+
+    def theme
+      current_theme
+    end
 
   end
 end
